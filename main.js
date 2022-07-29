4622f6f2ae8d50dd2ac87dcf03fb8e489f6bae97
zenoo97 <85465356+zenoo97@users.noreply.github.com>
2022-07-29 23:35:51 +0900

    ver1

---
+++ main.js
@@ -0,0 +1,10 @@
+const btnCount = 0;
+const downBtnClick = () => {
+  if (btnCount === 0) {
+    document.querySelector(".specInfo").style.display = "block";
+    btnCount = 1;
+  } else {
+    document.querySelector(".specInfo").style.display = "-webkit-box";
+    btnCount = 0;
+  }
+};
